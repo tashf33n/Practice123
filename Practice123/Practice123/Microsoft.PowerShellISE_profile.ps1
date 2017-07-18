6761b27b66b41f79fb9c2a05c7ff7065ee30e34d
tashf33n <tashfeen65@gmail.com>
2017-07-18 16:28:07 +0500

    Add files via upload

---
+++ Practice123/Practice123/Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,36 @@
+﻿function buildVS {
+
+        param
+        (
+        
+        [parameter(Mandatory=$true)]
+        [String] $path,
+
+        [parameter(Mandatory=$false)]
+        [bool] $nuget = $true,
+
+        [parameter(Mandatory=$false)]
+        [bool] $clean = $true
+        
+        )
+
+        process 
+        {
+            $msBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe'
+
+            if ($nuget) {
+            Write-Host "Restoring NuGet packages" -ForegroundColor Green
+            nuget restore "$($path)"            
+            }
+
+            if ($clean) {
+            Write-Host "Cleaning $($path)" -ForegroundColor Green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m
+            }
+
+            Write-Host "Building $($path)" -ForegroundColor Green
+            & "$($msBuildExe)" "$($path)" /t:Build /m
+
+        }
+
+}
